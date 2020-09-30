1207f537336b0e3d77e21b30780ab6347d1dac71
reginakhalil <khal4400@mylaurier.ca>
2020-09-30 14:19:10 -0400

    Add files via upload

---
+++ A112/src/cp372/GUI.java
@@ -0,0 +1,109 @@
+package cp372;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+
+@SuppressWarnings("serial")
+public class GUI extends JFrame {
+
+	public GUI() {
+		
+		setSize(660, 500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Book Storage Application");
+		
+		// Drop box elements
+		String[] requestOptions = {"GET","SUBMIT","UPDATE","REMOVE"};
+		JComboBox<String> dropBox = new JComboBox<String>(requestOptions);
+		
+		
+		// Labels 
+		JLabel ipLabel = new JLabel("IP:");
+		JLabel portLabel  = new JLabel("Port:");
+		JLabel dropLable = new JLabel("Requests Type:");
+		JLabel cientTextAreaLabel  = new JLabel("Request Message:"); 
+		JLabel serverTextAreaLable  = new JLabel("Resposne Message:");
+		
+		
+		// buttons 
+		JButton Submit  = new JButton("Send");
+		Submit.setEnabled(false);
+		JButton Disconnect  = new JButton("Disconnect");
+		Disconnect.setEnabled(false);
+		
+		JButton Connect = new JButton("Connect");
+		
+		
+		// buttons 
+		JButton submit  = new JButton("Send");
+		Submit.setEnabled(false);
+		JButton disconnect  = new JButton("Disconnect");
+		Disconnect.setEnabled(false);
+		
+		JButton connect = new JButton("Connect");
+
+		// check box 
+		JCheckBox all = new JCheckBox("All");
+		JCheckBox bibtex = new JCheckBox("BibTeX"); 
+
+		// text field 
+		JTextField ip = new JTextField(17);
+		JTextField port = new JTextField(17);
+
+		// text area 
+		JTextArea clientTextArea = new JTextArea(10, 50);
+		JTextArea serverTextArea = new JTextArea(10, 50);
+		serverTextArea.setEditable(false);
+		
+		// Panels
+		JPanel communcasPane = new JPanel();
+		communcasPane.setLayout(new BoxLayout(communcasPane,BoxLayout.Y_AXIS));
+		JPanel ipPanle = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JPanel newIpPanle = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JPanel communcationsPane = new JPanel();
+		communcationsPane.setLayout(new BoxLayout(communcationsPane,BoxLayout.Y_AXIS));
+		JPanel ClientPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JPanel SendPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		JPanel ServerPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		
+		// putting everything together 
+		ipPanle.add(portLabel);
+		ipPanle.add(port);
+		ipPanle.add(ipLabel);
+		ipPanle.add(ip);
+		ipPanle.add(Connect);
+		ipPanle.add(Disconnect);
+		newIpPanle.add(dropLable);
+		newIpPanle.add(dropBox);
+		newIpPanle.add(Submit);
+		newIpPanle.add(all);
+		newIpPanle.add(bibtex);
+		communcasPane.add(ipPanle);
+		communcasPane.add(newIpPanle);
+		
+		add(communcasPane, BorderLayout.NORTH);
+		
+		ClientPanel.add(cientTextAreaLabel);	
+		communcationsPane.add(ClientPanel);
+		communcationsPane.add(new JScrollPane(clientTextArea));
+		communcationsPane.add(SendPanel);
+		ServerPanel.add(serverTextAreaLable);
+		communcationsPane.add(ServerPanel);
+		communcationsPane.add(new JScrollPane(serverTextArea));
+		
+		add(communcationsPane);
+
+	}
+}
---
+++ A112/src/cp372/Main.java
@@ -0,0 +1,16 @@
+package cp372;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		GUI clientgui = new GUI(); 
+//		clientgui.setVisible(true);
+		boolean valid = Client.validateIsbn("978-3-16-148410-0"); 
+		System.out.print(valid);
+	
+	}
+
+}
