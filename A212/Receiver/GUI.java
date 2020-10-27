4c42a6cc605004d7876d5cff8e7006eb27a9c018
Regina Khalil <khal4400@mylaurier.ca>
2020-10-26 21:21:38 -0400

    GUI added

--- A212/Receiver/GUI.java
+++ A212/Receiver/GUI.java
@@ -1,9 +1,6 @@
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.WindowEvent;
-import javax.swing.*;
+import javax.swing.*; 
+import java.awt.*; 
+import java.awt.event.*; 
 
 /* Relies on the following files to be in default package:
  * 	Receiver
@@ -16,77 +13,141 @@ public class GUI extends JFrame
 		new GUI();
 	}
 
+	private Container c;
+	private JLabel title;
+	private JLabel host; 
+	private JLabel sender_udp;
+	private JLabel reciever_udp;
+
+	private JLabel file; 
+	private JLabel recieved_pckts; 
+	
+
+	private JTextField host_text; 
+	private JTextField sender_udp_text;
+	private JTextField reciever_udp_text;
+	private JTextField file_text;
+	private JTextArea recieved_pckts_text;
+
+	private JButton recieved; 
+
+	private JCheckBox rdt;   //reliable data transfer
+	private JCheckBox urdt;  //unreliable data transfer
+
 	public GUI()
 	{   	
-		setSize(660, 200);
-		setResizable(false);
-		setVisible(true);
-		initialize();
+		//main frame layout
+		setTitle("Reliable Data Transfer"); 
+		setBounds(200, 90, 500, 400); 
+        setDefaultCloseOperation(EXIT_ON_CLOSE); 
+        setResizable(false);
+        
 
+        //form layout
+        c = getContentPane(); 
+        c.setLayout(null); 
+
+        //Title 
+        title = new JLabel("Reliable Data Transfer"); 
+        title.setFont(new Font("Arial", Font.PLAIN, 20)); 
+        title.setSize(300, 30); 
+        title.setLocation(146, 5); 
+        c.add(title); 
+
+        //Host addres of the sender input
+        host = new JLabel("Host IP: "); 
+        host.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        host.setSize(100, 20); 
+        host.setLocation(70, 50); 
+        c.add(host); 
+
+        host_text = new JTextField(); 
+        host_text.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        host_text.setSize(190, 20); 
+        host_text.setLocation(170, 50); 
+        c.add(host_text);
+
+
+       	//UDP port of sender
+        sender_udp = new JLabel("Sender Port: "); 
+        sender_udp.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        sender_udp.setSize(100, 20); 
+        sender_udp.setLocation(70, 100); 
+        c.add(sender_udp); 
+
+        sender_udp_text = new JTextField(); 
+        sender_udp_text.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        sender_udp_text.setSize(190, 20); 
+        sender_udp_text.setLocation(170, 100); 
+        c.add(sender_udp_text);
+
+
+       	//UDP port of reciever
+        reciever_udp = new JLabel("Reciever Port: "); 
+        reciever_udp.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        reciever_udp.setSize(100, 20); 
+        reciever_udp.setLocation(70, 150); 
+        c.add(reciever_udp); 
+
+        reciever_udp_text = new JTextField(); 
+        reciever_udp_text.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        reciever_udp_text.setSize(190, 20); 
+        reciever_udp_text.setLocation(170, 150); 
+        c.add(reciever_udp_text);
+
+
+       	//File name
+        file = new JLabel("File Name: "); 
+        file.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        file.setSize(100, 20); 
+        file.setLocation(70, 200); 
+        c.add(file); 
+
+        file_text = new JTextField(); 
+        file_text.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        file_text.setSize(190, 20); 
+        file_text.setLocation(170, 200); 
+        c.add(file_text);
+
+        //Reliable Data transfer Checkbox
+        rdt = new JCheckBox("Reliable Data Transfer"); 
+        rdt.setFont(new Font("Arial", Font.PLAIN, 13)); 
+        rdt.setSize(190, 20); 
+        rdt.setLocation(70, 250); 
+        c.add(rdt);
+
+        //unreliable Data transfer Checkbox
+        urdt = new JCheckBox("Unreliable Data Transfer"); 
+        urdt.setFont(new Font("Arial", Font.PLAIN, 13)); 
+        urdt.setSize(190, 20); 
+        urdt.setLocation(70, 280); 
+        c.add(urdt);
+
+        //Recieve button
+        recieved = new JButton("Recieve"); 
+        recieved.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        recieved.setSize(90, 20); 
+        recieved.setLocation(300, 250);
+        c.add(recieved); 
+
+        // Number of recieved in-order packets
+        recieved_pckts = new JLabel("Number of Recieved Packets: "); 
+        recieved_pckts.setFont(new Font("Arial", Font.PLAIN, 13)); 
+        recieved_pckts.setSize(200, 20); 
+        recieved_pckts.setLocation(70, 320); 
+        c.add(recieved_pckts); 
+
+        recieved_pckts_text = new JTextArea(); 
+        recieved_pckts_text.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        recieved_pckts_text.setSize(100, 20); 
+        recieved_pckts_text.setLocation(250, 320); 
+        recieved_pckts_text.setLineWrap(true); 
+        recieved_pckts_text.setEditable(false); 
+        c.add(recieved_pckts_text); 
+
+        setVisible(true);
 
 	
-	}
-	public void initialize() 
-	{
-			//Thread receiver;
-		JTextArea packetsText = new JTextArea(1, 15);
-		JButton Transfer;
-	
-		JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		JLabel senderAddrLabel  = new JLabel("Sender IP address:");
-
-		JLabel receiverPortLabel  = new JLabel("Receiver port:         ");
-		JLabel outputFile  = new JLabel("Output file:");
-		JLabel Unreliable   = new JLabel("Unreliable transport:");
-		JLabel senderPort  = new JLabel("Sender port:");
-		JLabel Packets  = new JLabel("Number of accepted packets:");
-
-		// check box 
-		JCheckBox UnreliableCheckBox  = new JCheckBox("");
-
-		// button 
-		Transfer = new JButton("Transfer");
-
-		// text field 
-		JTextField senderAddr = new JTextField(17);
-		JTextField receiverPort = new JTextField(17);
-		JTextField file = new JTextField(17);
-		JTextField SenderPortText = new JTextField(17);
-
-		packetsText.setEditable(false);
-
-		JPanel ipPanle1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		JPanel ipPanle2 = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		JPanel ipPanle3 = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		JPanel ipPanle4 = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		JPanel ipPanle5 = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		JPanel ipPanle6 = new JPanel(new FlowLayout(FlowLayout.LEFT));
-
-		ipPanle1.add(senderAddrLabel);
-		ipPanle1.add(senderAddr);
-		ipPanle1.add(senderPort);
-		ipPanle1.add(SenderPortText);
-		ipPanle2.add(receiverPortLabel);
-		ipPanle2.add(receiverPort);
-		ipPanle3.add(outputFile);
-		ipPanle3.add(file);
-		ipPanle4.add(Unreliable);
-		ipPanle4.add(UnreliableCheckBox);
-		ipPanle5.add(Transfer);
-		ipPanle6.add(Packets);
-		ipPanle6.add(packetsText);
-
-		
-		panel.add(ipPanle1);
-		ipPanle4.setPreferredSize(new Dimension(ipPanle1.getPreferredSize().width, ipPanle1.getPreferredSize().height));
-		panel.add(ipPanle2);
-		panel.add(ipPanle3);
-		panel.add(ipPanle4);
-		panel.add(ipPanle5);
-		panel.add(ipPanle6);
-
-		add(panel);
-
 	}
 
 }
